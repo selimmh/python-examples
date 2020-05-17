4c5137b54eca2955d894d3c5ddcdfe0cb30dea45
selimmh <selim.mht@gmail.com>
2020-05-17 23:04:43 +0300

    initial commit

---
+++ 15-powers-of-integer.py
@@ -0,0 +1,10 @@
+# This program calculates the powers of given integer
+
+num = int(input("Number: "))
+terms = int(input("Terms: "))
+
+answer = list(map(lambda x: num ** x, range(terms)))
+
+print("The total terms are:",terms)
+for i in range(terms):
+   print(num,"^",i,"=",answer[i])

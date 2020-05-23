9a10a52f42ea1f8979c42d09448f720539901e34
selimmh <selim.mht@gmail.com>
2020-05-23 23:35:46 +0300

    initial commit

--- 23-perfect-number.py
+++ 23-perfect-number.py
@@ -1,7 +1,8 @@
 # This programs tells if number is perfect or not
-# Perfect number is a positive integer that is equal to the sum of its proper divisors 
+# Perfect number is a positive integer that is equal to the sum of its proper divisors
+# Some perfect numbers: 6, 28, 496, 8128, 33550336, 8589869056, 137438691328
 
-def perfect():
+def perfect_num():
     sum = 0
     for i in range(1,num):
         if num % i == 0:
@@ -10,7 +11,7 @@ def perfect():
     if sum == num:
         print(num, "is perfect number")
     else:
-        print(num, "is not perfect")
+        print(num, "is not perfect number")
 
-num = int(input("Number: "))
-perfect()
+num = int(input("Enter number: "))
+perfect_num()
